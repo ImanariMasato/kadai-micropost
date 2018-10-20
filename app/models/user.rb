57c91ba6c7cfe681d3ffb8c2fc63fa33e9997d15
taro.tanaka <reon09032@gmail.com>
2018-10-20 06:40:44 +0000

    favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,9 @@ class User < ApplicationRecord
   has_many :reverses_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationships, source: :user
   
+  has_many :favorites, dependent: :destroy
+  has_many :likes, through: :favorites, source: :micropost, dependent: :destroy
+  
   #relationshps
   def follow(other_user)
     unless self == other_user
@@ -32,5 +35,21 @@ class User < ApplicationRecord
   #toppage
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+  
+  #favorite
+  def favorite(micropost)
+    unless self.microposts == micropost
+      self.favorites.find_or_create_by(micropost_id: micropost.id)
+    end
+  end
+  
+  def unfavorite(micropost)
+    favorite = self.favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  def like?(micropost)
+    self.likes.include?(micropost)
   end
 end
